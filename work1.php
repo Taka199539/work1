485b35f131c434d381c80f7c1c0b2c0e3f4d4563
Takayuki <taka19950309@gmail.com>
2020-10-17 08:04:40 +0000

    課題１

--- work1.php
+++ work1.php
@@ -1,1 +1,46 @@
-{"filter":false,"title":"work1.php","tooltip":"/gitsample/work1.php","undoManager":{"mark":-1,"position":-1,"stack":[]},"ace":{"folds":[],"scrolltop":424,"scrollleft":0,"selection":{"start":{"row":44,"column":27},"end":{"row":44,"column":27},"isBackwards":false},"options":{"guessTabSize":true,"useWrapMode":false,"wrapToView":true},"firstLineState":{"row":25,"state":"php-start","mode":"ace/mode/php"}},"timestamp":1602858573145,"hash":"962454bf813e1bcbb23bb87a3263f5f364d3ba2d"}
+<?php 
+
+//1. 変数$aに3、$bに7を代入し、$a + $bの結果を表示する。
+$a = 3;
+$b = 7;
+
+echo $a + $b;
+
+
+//2. 配列$array_monthに1月〜12月の文字列を代入し、echoで8月を表示。
+$array_month = ["１月","2月","3月","4月","5月","6月","7月","8月","9月",
+"10月","11月","12月"];
+
+echo $array_month[7];
+
+
+//3. 変数$helloに"Hello,"、$nameに自分の名前、$worldに"`s World!"と表示。
+$hello = "Hello,";
+$name = "Takayuki";
+$world = "`s World!";
+echo $hello . $name . $world;
+
+
+//4. $tech_boostに"tech"という文字列ある。複合演算で「tech boost」と表示。
+$tech_boost = "tech";
+$tech_boost .= " boost";
+echo $tech_boost;
+
+
+//5. バグ修正
+$calendar = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+
+echo $calendar["December"];
